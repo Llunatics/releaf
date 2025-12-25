080eae0dfaddfd9c2a16cd534569c85d0d90e8d0
Muhamad Anggara Ramadhan <muh.anggaraaa@gmail.com>
2025-12-25 17:51:33 +0700

    fix: Set minSdk to 23 for camera-core compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.informatika.releaf"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion  // Required by camera-core library
+        minSdk = 23  // Required by camera-core library
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
